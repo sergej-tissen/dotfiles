a8a5310df082b3547a5b9389db59eeca72f1f910
Sergej Tisen <sergej.tissen@gmail.com>
2016-07-21 09:05:32 +0200

    renamed .dotfiles to dotfiles

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-ln -sv ~/.dotfiles/.inputrc ~
-ln -sv ~/.dotfiles/.gitconfig ~
-ln -sv ~/.dotfiles/.bash_profile ~
-ln -sv ~/.dotfiles/.vimrc ~
-ln -sv ~/.dotfiles/git-completion.bash ~
-ln -sv ~/.dotfiles/.tern-config ~
-ln -sv ~/.dotfiles/.bashrc ~
-ln -sv ~/.dotfiles/.tmux.conf ~
+ln -sv ~/dotfiles/.inputrc ~
+ln -sv ~/dotfiles/.gitconfig ~
+ln -sv ~/dotfiles/.bash_profile ~
+ln -sv ~/dotfiles/.vimrc ~
+ln -sv ~/dotfiles/git-completion.bash ~
+ln -sv ~/dotfiles/.tern-config ~
+ln -sv ~/dotfiles/.bashrc ~
+ln -sv ~/dotfiles/.tmux.conf ~
 mkdir ~/.vim/.swapfiles
 mkdir ~/.vim/.undo
